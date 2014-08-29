c9b96740f06ba96bd50533fbd5618c5bdc0525ed
jslootweg <Corrado11dropbox>
2014-08-29 20:57:42 +0100

    * BufferedMemoryStreamTest-NUnit.csproj: Added NUnit test project.
    * BufferedMemoryStream.sln:
    * Test.cs: Added NUnit test project.

---
+++ BufferedMemoryStreamTest-NUnit/Test.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace BufferedMemoryStreamTestNUnit
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			using (BufferedMemoryStream.BufferedMemoryStream memStream = new BufferedMemoryStream.BufferedMemoryStream())
+			{
+				byte[] buffer = new byte[2];
+				buffer[0] = 1;
+				buffer[1] = 2;
+				int bytesRead = 2;
+
+				memStream.Write(buffer, 0, bytesRead);
+
+				buffer[0] = 3;
+				buffer[1] = 4;
+
+				memStream.Write(buffer, 0, bytesRead);
+
+				byte[] result = memStream.ToArray();
+
+				Assert.AreEqual(4, memStream.Length, "Stream has invalid length");
+
+				CollectionAssert.AreEquivalent(new byte[] { 1, 2, 3, 4 }, result);
+			}
+		}
+	}
+}
+
